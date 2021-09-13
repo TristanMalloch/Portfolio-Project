1155e88b11d729e1161e2f2d196b44319afa873e
Tristan Malloch <tristanmalloch@protonmail.com>
2021-09-13 20:23:57 +1000

    add mobile menu

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,5 +1,7 @@
 let dogFightName = document.getElementById('dogFightName');
 let dogFightDetails = document.getElementById('dogFightDetails');
+let mobileNav = document.getElementById('smallNav');
+let hMenuIcon = document.getElementById('hMenuIcon');
 
 dogFightName.onmouseover
 
@@ -12,3 +14,13 @@ let showDogFight = function() {
 };
 
 dogFightName.onclick = showDogFight;
+
+let showMobileNav = function() {
+	if (mobileNav.style.display == "inline-block") {
+		mobileNav.style.display = "none";
+	} else {
+		mobileNav.style.display = "inline-block";
+	}
+};
+
+hMenuIcon.onclick = showMobileNav;
